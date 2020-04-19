89e3dfdc97ecc1f1f7a28998c5df4475ab82c9ce
Tatiana <tatiana_ivanova89@mail.ru>
2020-04-19 17:05:46 +0700

    добавлено значение по умолчанию

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@ const renderGoodsItem = (title, price) => {
   return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 };
 
-const renderGoodsList = (list) => {
+const renderGoodsList = (list = goods) => { //добавлено значение по умолчанию
   let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
   document.querySelector('.goods-list').innerHTML = goodsList.join(''); //по умолчанию элементы массива разделяются запятой, с помощью метода join преобразуем элементы массива в строку, а в качестве разделителя используем указанный в скобках параметр
 }
 
-renderGoodsList(goods);
+renderGoodsList();
