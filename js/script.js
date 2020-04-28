906f2db59f219b6a90b192051e1662d7b131c0f5
Tatiana <tatiana_ivanova89@mail.ru>
2020-04-28 19:27:30 +0700

    Revert "методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины."
    This reverts commit 4f163eb5467b4a1934048912927f896257f72519.

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@
 function makeGETRequest(url, callback) {
   var xhr;
 
-  	if (window.XMLHttpRequest) {//Chrome, Mozilla, Opera, Safari
-    	xhr = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {//Internet Explorer
-    	xhr = new ActiveXObject("Microsoft.XMLHTTP");
-  	};
+  if (window.XMLHttpRequest) {//Chrome, Mozilla, Opera, Safari
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {//Internet Explorer
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  };
 
- 	xhr.onreadystatechange = function () {//ловим момент, когда ответ сервиса получен
-    	if (xhr.readyState === 4) {
-      	callback(xhr.responseText);//выполнится после получения ответа
-    	};
-  	};
+  xhr.onreadystatechange = function () {//ловим момент, когда ответ сервиса получен
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);//выполнится после получения ответа
+    };
+  };
 
   xhr.open('GET', url, true);//тип запроса, адрес ресурса, указатель асинхронности
   xhr.send();//метод для отправки запроса
@@ -41,10 +41,24 @@ class GoodsList {
     	makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
       		this.goods = JSON.parse(goods);
       		cb();
-			//console.log(this.goods);
     	});
   };
 
+  	/*fetchGoods() {
+    	this.goods = [
+      		{ title: 'Shirt', price: 150 },
+      		{ title: 'Socks', price: 50 },
+      		{ title: 'Jacket', price: 350 },
+      		{ title: 'Shoes', price: 250 },
+    	];
+		
+	  	this.goods.forEach(good => {
+	  		const goodItem = new GoodsItem(good.title, good.price);
+	  		console.log(goodItem);
+			listHtml += goodItem.render();
+			sum+=good.price;
+	  	});
+  	};*/
   	render() {
 		let listHtml = '';
 		this.goods.forEach(good => {
@@ -57,10 +71,10 @@ class GoodsList {
   	};
 	
 //Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
-	/*cost() {
+	cost() {
 		//sum+=this.price;
 		console.log ('общая стоимость товаров составляет: ' + sum);
-	}*/
+	}
 };
 
 
@@ -69,76 +83,43 @@ let sum = 0;
 const list = new GoodsList();
 list.fetchGoods(() => {
   	list.render();
-	//list.cost();
+	list.cost();
 });
 
 //Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.
 
-class GoodsListBasket {
+class GoodsListBasket extends GoodsList {
 	//метод для формирования номера заказа
+	//ДОБАВЛЕНО ЗНАЧЕНИЕ ПО УМОЛЧАНИЮ В ДОПОЛНЕНИЕ К ДЗ_1
 	num (number = 1) {
 		return number;
 	};
 	
-	//получение списка товаров в корзине
-	getBasket(cb) {
-		makeGETRequest(`${API_URL}/getBasket.json`, (goods) => {
-      		this.goods = JSON.parse(goods);
-      		cb();
-			console.log(this.goods);
-			this.goods.contents.forEach(good => {
-	  			const goodItem = new GoodsItem(good.product_name, good.price);
-	  			console.log(goodItem.product_name);
-	  		});
-    	});
-	};
-	
-	//метод для добавления товара в корзину
-	addItemToBasket(product_name, price) {		
-    	makeGETRequest(`${API_URL}/addToBasket.json`, (addItem) => {
-      		this.goods.contents.push(new GoodsItem(product_name, price));
-			console.log(this.goods);
-			console.log(this.goods.contents.length);
-    	});
-	};
-
-	//метод, определяющий суммарную стоимость всех товаров в корзине
+	//метод для определения общего кол-ва товаров в корзине
 	amount() {};
 	
-	//метод, определяющий количество товаров в корзине
-	countGoods(cb) {
-		/*cb();
-		let count = this.goods.contents.length;
-		console.log('В корзине ' + count + ' товара');*/
-	};
+	//метод, определяющий суммарную стоимость всех товаров в корзине
+	cost() {};
 	
 	//метод для оформления заказа
 	put() {}; 
 	
 	//метод для удаления всех товаров из корзины
-	clearBasket() {
-		makeGETRequest(`${API_URL}/deleteFromBasket.json`, (del) => {
-      		this.goods.contents.splice(0, this.goods.contents.length);
-			console.log(this.goods.contents);
-    	});
-	};
+	clear() {};
 	
 };
 
-class GoodsItemBasket extends GoodsItem {};
+const listBasket = new GoodsListBasket();
+console.log('Номер заказа: ' + listBasket.num(3));
 
-
-const basket = new GoodsListBasket();
-basket.getBasket(() => {
-	basket.amount();
-	//basket.countGoods(() => {
-		basket.addItemToBasket('клавиатура', 1000);
-		basket.addItemToBasket('колонки', 10000);
-	//});
-	basket.clearBasket();
-  	//basket.render();
-	//basket.cost();
+class GoodsItemBasket extends GoodsItem {
+	//метод для изменения количества данного типа товара
+	setAmountItem() {};
 	
-});
+	//метод для удаления выбранного товара из корзины
+	delItem() {};
+};
+
+
 
 
