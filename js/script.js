ebd6ab0fb7e8f99b4f3174dd9c49b9d2566123ad
Tatiana <tatiana_ivanova89@mail.ru>
2020-05-20 19:45:41 +0700

    Заглушка

--- js/script.js
+++ js/script.js
@@ -76,5 +76,27 @@ const app = new Vue({
 	}
 });
 
+/*
+Vue.component ('goods-list', {
+	props: ['goods'],
+  	template: `
+    	<div class="goods-list">
+      		<goods-item v-for="good in goods" :good="good"></goods-item>
+    	</div>
+  		`
+});
 
+Vue.component ('good-item', {
+	  props: ['good'],
+  template: `
+    <div class="goods-item">
+      <h3>{{ good.product_name }}</h3>
+      <p>{{ good.price }}</p>
+    </div>
+  `
+});*/
+
+Vue.component('some-component', {
+	template: '<h1><slot></slot></h1>'
+  });
 
